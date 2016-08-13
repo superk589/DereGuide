9d98bb43879c25b9d56cd854189a4351f27fac62
zzk <superk521@foxmail.com>
2016-08-13 16:37:42 +0800

    add toolbox icons

---
+++ CGSSGuide/Toolbox/View/ToolboxTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToolboxTableViewCell.swift
+//  CGSSGuide
+//
+//  Created by zzk on 16/8/13.
+//  Copyright © 2016年 zzk. All rights reserved.
+//
+
+import UIKit
+
+class ToolboxTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
