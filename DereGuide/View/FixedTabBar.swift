5bcc9543cff962efe0f52a03fb94e283f94e8b97
superk <superk589@gmail.com>
2018-01-11 10:36:02 +0800

    fix tab bar animation when pop from the secondary to root

---
+++ DereGuide/View/FixedTabBar.swift
@@ -0,0 +1,53 @@
+//
+//  FixedTabBar.swift
+//  DereGuide
+//
+//  Created by zzk on 11/01/2018.
+//  Copyright © 2018 zzk. All rights reserved.
+//
+
+import UIKit
+
+class SafeAreaFixTabBar: UITabBar {
+    
+    var oldSafeAreaInsets = UIEdgeInsets.zero
+    
+    @available(iOS 11.0, *)
+    override func safeAreaInsetsDidChange() {
+        super.safeAreaInsetsDidChange()
+        
+        if oldSafeAreaInsets != safeAreaInsets {
+            oldSafeAreaInsets = safeAreaInsets
+            
+            invalidateIntrinsicContentSize()
+            superview?.setNeedsLayout()
+            superview?.layoutSubviews()
+        }
+    }
+    
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        var size = super.sizeThatFits(size)
+        if #available(iOS 11.0, *) {
+            let bottomInset = safeAreaInsets.bottom
+            if bottomInset > 0 && size.height < 50 && (size.height + bottomInset < 90) {
+                size.height += bottomInset
+            }
+        }
+        return size
+    }
+    
+    override var frame: CGRect {
+        get {
+            return super.frame
+        }
+        set {
+            var tmp = newValue
+            if let superview = superview, tmp.maxY !=
+                superview.frame.height {
+                tmp.origin.y = superview.frame.height - tmp.height
+            }
+            
+            super.frame = tmp
+        }
+    }
+}
